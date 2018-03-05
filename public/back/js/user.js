227388bf001a812e13149a52f04921a8cfdf71d5
chenguozhu11 <1648667333@qq.com>
2018-03-05 09:22:41 +0800

    user基本完成

--- public/back/js/user.js
+++ public/back/js/user.js
@@ -1,9 +1,10 @@
 $(function () {
+
+    var currentPage = 1;
+    var pageSize = 5;
+    var flag = true;
+
     function rander() {
-        var currentPage = 1;
-        var pageSize = 5;
-        console.log(currentPage);
-        
         $.ajax({
             type: "get",
             url: "/user/queryUser",
@@ -12,8 +13,29 @@ $(function () {
                 pageSize: pageSize
             },
             success: function (info) {
-                console.log(info);
+                $("table>tbody").html(template("tmp", info));
+                var totalPages = Math.ceil(info.total / info.size);
+                if (flag) {
+                    fllag = false;
+                    $("#pagintor").bootstrapPaginator({
+                        bootstrapMajorVersion: 3,
+                        currentPage: currentPage,
+                        totalPages: totalPages,
+                        size: "small", //设置控件的大小，mini, small, normal,large
+                        onPageClicked: function (event, originalEvent, type, page) {
+                            currentPage = page;
+                            rander();
+                        }
+                    });
+                }
             }
         })
     }
+    rander();
+    $("tbody").on("click",".btn",function(){
+        var isDelete=$(this).parent().data("isD");
+        var id=$(this).parent().data("id");
+       
+        
+    })
 })
